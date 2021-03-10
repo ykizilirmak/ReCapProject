5a1c63a5bdd537879c9fec3f85a9a79d98986909
ykizilirmak <ykizilirmak93@gmail.com>
2021-03-10 16:59:45 +0300

    FluentValidationApplied

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -12,6 +12,12 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(p => p.Description).NotEmpty();
             RuleFor(p => p.Description).MinimumLength(2);
+            RuleFor(p => p.BrandId).NotEmpty();
+            RuleFor(p => p.BrandId).GreaterThan(0);
+            RuleFor(p => p.ColorId).NotEmpty();
+            RuleFor(p => p.ColorId).GreaterThan(0);
+            RuleFor(p => p.ModelYear).NotEmpty();
+            RuleFor(p => p.ModelYear).GreaterThan(0);
             RuleFor(p => p.DailyPrice).NotEmpty();
             RuleFor(p => p.DailyPrice).GreaterThan(0);
            // RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
